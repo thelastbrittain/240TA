76c3532bdff290fdd947cb199175155a11e4b82b
thelastbrittain <benjaminpbrittain@gmail.com>
2025-01-09 17:27:53 -0700

    Change project directory flow

---
+++ shared/src/main/java/chess/ChessMove.java
@@ -0,0 +1,67 @@
+package chess;
+
+import java.util.Objects;
+
+/**
+ * Represents moving a chess piece on a chessboard
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessMove {
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece;
+
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
+                     ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * @return ChessPosition of starting location
+     */
+    public ChessPosition getStartPosition() {
+        return startPosition;
+    }
+
+    /**
+     * @return ChessPosition of ending location
+     */
+    public ChessPosition getEndPosition() {
+        return endPosition;
+    }
+
+    /**
+     * Gets the type of piece to promote a pawn to if pawn promotion is part of this
+     * chess move
+     *
+     * @return Type of piece to promote a pawn to, or null if no promotion
+     */
+    public ChessPiece.PieceType getPromotionPiece() {
+        return promotionPiece;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessMove{" +
+                "startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                ", promotionPiece=" + promotionPiece +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
+}
---
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -0,0 +1,109 @@
+package chess;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class PawnMoveCalculator {
+    private ChessBoard board;
+    private ChessPosition position;
+    private ChessGame.TeamColor teamColor;
+    private Collection<ChessMove> ogList;
+    private int row;
+    private int col;
+    private List<ChessPiece.PieceType> promotionPieces = Arrays.asList(ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.ROOK, ChessPiece.PieceType.BISHOP);
+
+    public PawnMoveCalculator(ChessBoard board, ChessPosition position, ChessGame.TeamColor teamColor, Collection<ChessMove> ogList) {
+        this.board = board;
+        this.position = position;
+        this.teamColor = teamColor;
+        this.ogList = ogList;
+        this.row = position.getRow();
+        this.col = position.getColumn();
+    }
+
+    //the logic.
+    public void calculateMoves() {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            goDirection(1,0);
+            goDirection(1,1);
+            goDirection(1,-1);
+        } else {
+            goDirection(-1, 0);
+            goDirection(-1, 1);
+            goDirection(-1,-1);
+        }
+    }
+
+    //this function takes in a location. If an enemy or nothing is there, it adds 4 (5?) moves to the list, Knight, Rook, Bishop, Queen, Pawn?
+    private void promotePiece(ChessPosition newPosition) {
+        for (ChessPiece.PieceType piece : promotionPieces) {
+            ChessMove move = new ChessMove(position, newPosition, piece);
+            ogList.add(move);
+        }
+    }
+
+    private void goDirection(int rowChange, int colChange) {
+        //create new row and col
+        int newRow = row + rowChange;
+        int newCol = col + colChange;
+        //make sure new rol/col are in bounds
+        if (newRow > 8 || newCol > 8 || newRow < 1 || newCol < 1) {
+            return;
+        }
+
+        //creating variables for promotion/doubleMove
+        boolean doubleMove = false;
+        boolean promotion = false;
+        //setting the promotion and double move bools
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            if (row == 2 && colChange == 0) {
+                doubleMove = true;
+            }
+            if (row == 7) {
+                promotion = true;
+            }
+        }
+        if (teamColor == ChessGame.TeamColor.BLACK) {
+            if (row == 7 && colChange == 0) {
+                doubleMove = true;
+            }
+            if (row == 2) {
+                promotion = true;
+            }
+        }
+
+        //make new position
+        //get the piece at the new position
+        ChessPosition newPosition = new ChessPosition(newRow, newCol);
+        ChessPiece newPiece = board.getPiece(newPosition);
+
+        if (colChange == 0) {
+            if (newPiece == null) {
+                if (promotion) {
+                    promotePiece(newPosition);
+                } else {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    ogList.add(newMove);
+                    if (doubleMove) {
+                        ChessPosition doublePosition = new ChessPosition(newRow + rowChange, newCol);
+                        ChessPiece testPiece = board.getPiece(doublePosition);
+                        if (testPiece == null) {
+                            ChessMove newDoubleMove = new ChessMove(position, doublePosition, null);
+                            ogList.add(newDoubleMove);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (newPiece != null && newPiece.getTeamColor() != teamColor) {
+                if (promotion) {
+                    promotePiece(newPosition);}
+                else {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    ogList.add(newMove);
+                }
+            }
+        }
+    }
+}
